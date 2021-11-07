5cd4c8decdd967eb6b95055fff3654cdb0a3b40e
Tony N <tony.phuongtu@setel.com>
2021-11-07 18:09:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-$(document).ready(function () {
+// $(document).ready(function () {
+//
+// });
+
+function ready(callback) {
+  // in case the document is already rendered
+  if (document.readyState != "loading") callback();
+  // modern browsers
+  else if (document.addEventListener)
+    document.addEventListener("DOMContentLoaded", callback);
+  // IE <= 8
+  else
+    document.attachEvent("onreadystatechange", function () {
+      if (document.readyState == "complete") callback();
+    });
+}
+
+ready(function () {
   let oldToken = "";
   if (localStorage.getItem("oldToken") !== null) {
     oldToken = localStorage.getItem("oldToken");
@@ -6,17 +23,26 @@ $(document).ready(function () {
 
   setInterval(
     (function getToken() {
-      $.get("https://kungfu.minhminh13.repl.co", function (data) {
-        console.log("Checking token!!!!");
-        if (oldToken !== data) {
-          console.log("Adding new token!!!!");
-          oldToken = data;
-          localStorage.setItem("oldToken", oldToken);
-          localStorage.setItem("api_token", oldToken);
-          localStorage.setItem("expires_in", +new Date() / 1000 + 86000);
-          location.reload();
-        }
-      });
+      var opts = {
+        method: "GET",
+        headers: {},
+      };
+      fetch("https://kungfu.minhminh13.repl.co", opts)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (body) {
+          console.log("Checking token!!!!");
+          console.log(body);
+          if (oldToken !== body) {
+            // console.log("Adding new token!!!!");
+            // oldToken = data;
+            // localStorage.setItem("oldToken", oldToken);
+            // localStorage.setItem("api_token", oldToken);
+            // localStorage.setItem("expires_in", +new Date() / 1000 + 86000);
+            // location.reload();
+          }
+        });
     })(),
     1000
   );
